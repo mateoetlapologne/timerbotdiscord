c84d6cffd386a8945fd8e841ca43ed871de9938c
vroum vroum <113009625+mateoetlapologne@users.noreply.github.com>
2025-02-02 02:41:38 +0100

    Trad of the discord comments

--- main.py
+++ main.py
@@ -2,60 +2,39 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from datetime import datetime
-
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
-
-
-@bot.tree.command(name="cquand", description="Compte à rebours jusqu'au 23 décembre 2024 à 09:55.")
+@bot.tree.command(name="when", description="Discord command for know how many day/hours last before the day...")
 async def cquand(interaction: discord.Interaction):
-    # Date et heure cibles
+    # Date/Hour
     target_time = datetime.strptime("2024-12-23 09:55", "%Y-%m-%d %H:%M")
-
-    # Vérification du temps actuel
     now = datetime.now()
-
     if target_time <= now:
-        # Si la date est déjà passée
         await interaction.response.send_message(
-            "La date est déjà passée ! 🎉", ephemeral=True
+            "Day is already outpassed", ephemeral=True
         )
         return
-
-    # Calcul du temps restant
     remaining_time = target_time - now
     days, seconds = divmod(remaining_time.total_seconds(), 86400)
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
-
-    # Création de l'embed avec le temps restant
     embed = discord.Embed(
-        title="C'est quand qu'on y va ?",
-        description=f"Il reste :",
+        title="Change the title",
+        description=f"Time last",
         color=discord.Color.blue()
     )
     embed.add_field(
-        name="Temps restant",
+        name="Time Last",
         value=f"{int(days)} jours, {int(hours)} heures, {int(minutes)} minutes.",
         inline=False
     )
     embed.set_image(url="https://i.ibb.co/kmsBmcC/squidgame.gif")
-
-    # Envoi du message une seule fois
     await interaction.response.send_message(embed=embed)
-
-
-# Synchronisation des commandes slash et confirmation
 @bot.event
 async def on_ready():
     try:
         synced = await bot.tree.sync()
-        print(f"Commandes slash synchronisées : {synced}")
     except Exception as e:
-        print(f"Erreur lors de la synchronisation des commandes : {e}")
-    print(f"Bot connecté en tant que {bot.user}")
-
-
-# Démarrage du bot avec le toke
+    print(f"Bot ready{bot.user}")
         
-bot.run("")
+bot.run("") #Put the discord token here
